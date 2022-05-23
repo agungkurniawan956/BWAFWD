2626049bab263dbc4f037ee5654a2baece3d35e3
agungkurniawan956 <kurniawanagung956@gmail.com>
2022-05-23 11:14:07 +0700

    alhamdulillah udah masuk php dasar syntaks, variable dan tipe data

---
+++ belajar.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang='en'>
+
+<head>
+  <meta charset='UTF-8'>
+  <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+  <meta http-equiv='X-UA-Compatible' content='ie-edge'>
+  <title>Belajar PHP</title>
+</head>
+
+<body>
+  <?php
+  // Tipe Data
+  $a = "ini tipe data string"; //string
+  $b = 1000; //integer
+  $c = true; //bolean
+  $d = 10.5; //float
+  $e = ["Satu, dua, Tiga"]; //array
+  $f = null; //nul atau kosong
+
+  var_dump($f);
+
+
+
+
+  ?>
+</body>
+
+</html>
